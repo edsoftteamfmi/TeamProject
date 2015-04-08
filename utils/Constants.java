3be93ee32e8d20830ec9479836f1e09ac478eae4
edsoftteamfmi <edsoftteamfmi@gmail.com>
2015-04-08 18:03:39 +0300

    Create Constants.java

---
+++ utils/Constants.java
@@ -0,0 +1,61 @@
+package com.ereader.utils;
+
+
+public class Constants {
+	public static final int GLASNA_SYGLASNA = 1;
+	public static final int SYGLASNA_GLASNA = 2;
+	public static final int GLASNA_SYGLASNA_GLASNA = 3;
+	public static final int SYGLASNA_GLASNA_SYGLASNA = 4;
+	public static final int SYGLASNA_SYGLASNA_GLASNA = 5;
+	public static final int GLASNA_SYGLASNA_SYGLASNA = 6;
+	public static final int GLASNA_GLASNA_SYGLASNA = 7;
+	public static final int SYGLASNA_GLASNA_GLASNA = 8;
+	public static final int BORDER_STROKE = 2;
+	
+	//Models constants
+	public static final int ANGEL = 5;
+	public static final int ARM = 6;
+	public static final int BANANA = 7;
+	public static final int BED = 8;
+	public static final int BLOUSE = 9;
+	public static final int BOOK = 10;
+	public static final int CAT = 11;
+	public static final int CHAIR = 12;
+	public static final int CLOUD = 13;
+	public static final int COCK = 14;
+	public static final int COW = 15;
+	public static final int DOG = 16;
+	public static final int DOLL = 17;
+	public static final int EAR = 18;
+	public static final int ELEPHANT = 19;
+	public static final int EYE = 20;
+	public static final int FEATHER = 21;
+	public static final int FISH = 22;
+	public static final int FLOWER = 23;
+	public static final int FROG = 24;
+	public static final int HEART = 25;
+	public static final int HONEY = 26;
+	public static final int HORSE = 27;
+	public static final int IGLOO = 28;
+	public static final int LEG = 29;
+	public static final int LION = 30;
+	public static final int MOON = 31;
+	public static final int NOSE = 32;
+	public static final int PAW = 33;
+	public static final int PUMPKIN = 34;
+	public static final int PURSE = 35;
+	public static final int RABBIT = 37;
+	public static final int SCARF = 238;
+	public static final int SHIP = 39;
+	public static final int SUN = 40;
+	public static final int TABLE = 41;
+	public static final int THREE = 42;
+	public static final int TIGER = 43;
+	public static final int TOOTH = 44;
+	public static final int TREE = 45;
+	public static final int WALNUT = 46;
+	public static final int WATER = 47;
+	public static final int SMILE = 48;
+	public static final int SAD = 49;
+	
+}
